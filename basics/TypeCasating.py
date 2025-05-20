3c86cb5506b18845309aed701a62c51b9784ddc7
Jasmine Akter <59429546+Jasmine3014@users.noreply.github.com>
2025-05-20 16:14:19 +0600

    first commit

---
+++ basics/TypeCasating.py
@@ -0,0 +1,11 @@
+height = 5.9
+#TypeCasting
+print(type(height))
+res = int(height)
+print(res)
+print("Type of res is :",type(res))
+
+sum = "100.5"
+sum_num = float(sum)
+print(sum_num)
+print("Type is :",type(sum_num))
---
+++ basics/Variables.py
@@ -0,0 +1,37 @@
+#Int
+number = 10
+
+#Float
+height = 5.9
+
+#String
+name = "Jamine"
+
+#Boolean
+is_active = True
+
+#List
+results= ["A+", "A",70,80]
+
+#Tuples
+cordinate = (1,2,3)
+
+#Dictionary
+person = {
+    "name" : "Jasmine",
+    "university" : "BUBT",
+    "batch" : 23
+}
+
+#Set
+color = {"red","blue","yellow"}
+
+print("Integer: ",number)
+print("Float: ",height)
+print("String: ",name)
+print("Boolean: ",is_active)
+print(f"Integer : {number}")
+
+#Checking variable type
+print("Type of number is :",type(number))
+print(f"Type of {number} is : {type(number)}")
